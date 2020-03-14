2e52a950315e9fbe12a8f09cf25d414455bd3619
dgranadier <dg52993@gmail.com>
2020-03-14 05:52:35 +0000

    1

--- app.rb
+++ app.rb
@@ -14,5 +14,65 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+blog_table = DB.from(:blog)
+comments_table = DB.from(:comments)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+get "/" do
+    puts blog_table.all
+    @blog = blog_table.all.to_a
+    view "blog"
+end
+get "/blog/:id" do
+    @blog = blog_table.where(id: params[:id]).to_a[0]
+    @comments = comments_table.where(event_id: @comments[:id])
+    @like_count = comments_table.where(event_id: @like_count[:id], like: true).count
+    @users_table = users_table
+    view "blog"
+end
+
+get "/blog/:id/comments/new" do
+    @blog = blog_table.where(id: params[:id]).to_a[0]
+    view "new_comment"
+end
+
+get "/blog/:id/comments/create" do
+    puts params
+    @blog = blog_table.where(id: params["id"]).to_a[0]
+    comments_table.insert(blog_id: params["id"],
+                       user_id: session["user_id"],
+                       like: params["going"],
+                       comments: params["comments"])
+    view "create_comment"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    puts params
+    hashed_password = BCrypt::Password.create(params["password"])
+    users_table.insert(name: params["name"], email: params["email"], password: hashed_password)
+    view "create_comment"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    puts BCrypt::Password::new(user[:password])
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,50 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :blog do
   primary_key :id
   String :title
-  String :description, text: true
+  String :content, text: true
   String :date
-  String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :comments do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :blog_id
+  Boolean :like
   String :name
   String :email
   String :comments, text: true
 end
 
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :email
+  String :password
+end
+
 # Insert initial (seed) data
-events_table = DB.from(:events)
+events_table = DB.from(:blog)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+blog_table.insert(title: "Multi-tasking with Uber + Lyft", 
+                    description: "Driving with Uber and Lyft at the same time can be tricky, but many drivers find that it helps to maximize income when done right. Here are the best of the best tips we have seen from drivers who choose to use Uber and Lyft at the same time: 
+                    Don’t use 2 phones - it is distracting, often worrisome for the passenger, and usually not worth the extra cell phone payments.
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+                    Close out all other apps from running - Just running Uber and Lyft takes up A LOT of data, which can slow down your phone. Closing out all other apps helps to prevent your phone from crashing.
+
+                    Run Uber on the main screen - Uber closes out after a couple minute of inactivity, therefore, you need to have Uber open on the main screen. However, Lyft will keep running  in the background - allowing you to have both Uber and Lyft going at the same time.
+
+                    Choosing a ride - Oftentimes, when you have both running, you may get a ride from both Uber and Lyft simultaneously. Which ride should you choose? The one closest to you. This helps you save time between rides. The key to making more money is having a passenger in the car at all times.
+
+                    Log out immediately - Do not forget to close out of the other app (the one you did not accept a ride with).
+
+                    Tell us what YOU think about driving with both Uber and Lyft in the comments below!",
+                    date: "March 9")
+
+blog_table.insert(title: "DJ or Driver?", 
+                    description: "IHave you ever been jamming to music but a rider asks you turn it down? We’ve been there, too.  But it got us wondering, what is the best music to play for passengers during rides? We did some digging, and the answer might surprise you. 
+
+                    The most well-received music for passengers is … (drum roll please) … JAZZ! Passengers want their rides to be peaceful, and surprisingly, rap and top 40 pop songs don’t exactly make rides relaxing. 
+
+                    Whether you buy into the Jazz theory or not, here are some more tips for playing music during rides that will help you increase your driver rating and earn more tips:",
+                    date: "March 3")
